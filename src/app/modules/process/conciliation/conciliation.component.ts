1c00703aa0c2bd249f783593212ac0ddb6b15027
Jeitson Smith Guerrero Barajas <112034595+aldaizz@users.noreply.github.com>
2024-12-04 15:15:06 -0500

    .

--- src/app/modules/process/conciliation/conciliation.component.ts
+++ src/app/modules/process/conciliation/conciliation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api/api.service';
 
 declare var bootstrap: any;
 @Component({
@@ -12,7 +13,9 @@ export class ConciliationComponent implements OnInit {
   currentPage: number = 1; // Página actual
   itemsPerPage: number = 5; // Cantidad de elementos por página
   totalPages: number = 0; // Total de páginas
+  totalItems = 0;
   paginatedList: any = [];
+  listBase: any[] = [];
   modal: any;
   modalConfirm: any;
   listReceptions: any[] = [
@@ -26,21 +29,33 @@ export class ConciliationComponent implements OnInit {
       glosa: 240.000
     }
   ];
-  constructor(){
+  audit: any = {};
+  constructor(private api: ApiService){
   }
 
   ngOnInit(){
     this.modal = new bootstrap.Modal(document.getElementById('modaldetail'), {backdrop: 'static', keyboard: false})
     this.modalConfirm = new bootstrap.Modal(document.getElementById('modalConfirm'), {backdrop: 'static', keyboard: false})
-
+    this.getReceptions(this.currentPage);
   }
 
-  getReceptions(){
-
+  getReceptions(item: any){
+    this.api.get(`audit_guia?page=${item}`).subscribe({
+      next: (response: any) => {
+        this.listReceptions = response.data.items;
+        this.listBase = this.listReceptions; // Guardamos la lista original para filtrar
+        this.totalItems = this.listReceptions.length; // Total de solicitudes
+        this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage); // Total de página
+      },
+      error: (error: any) => {
+        console.error('Error al crear usuario:', error);
+      },
+    });
   }
 
   viewDetail(item: any[]){
     this.modal.show();
+    this.audit = item;
   }
 
   openModalConciliation(item: any[]){
@@ -50,6 +65,24 @@ export class ConciliationComponent implements OnInit {
   setAnswer(){
     this.modal.hide();
     this.modalConfirm.show();
+  }
+
+  setColorStatus(status: any){
+    let color = '';
+    switch (status) {
+      case '101': //sin guia
+        color = 'bg-danger'
+        break;
+      case '102': //pendiente
+        color = 'bg-warning';
+        break;
+      case '103': //confirmado
+        color = 'bg-success'
+        break;
+      default:
+        break;
+    }
+    return color;
   }
 
   actionConfirm(){
