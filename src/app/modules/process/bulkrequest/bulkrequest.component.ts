1b5d0cbe7d03e2143baf534caa0a9d0f3531ad4f
luis torres <luistorbul2002@gmail.com>
2025-02-10 10:44:32 -0500

    avances

--- src/app/modules/process/bulkrequest/bulkrequest.component.ts
+++ src/app/modules/process/bulkrequest/bulkrequest.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 import { GeneralService } from 'src/app/core/services/general.service';
 import { ToastService } from 'src/app/core/services/toast.service';
 import * as XLSX from 'xlsx';
@@ -16,6 +17,7 @@ export class BulkrequestComponent {
     referenciaWll: string;
     referenciaPh: string;
   }> = [];
+  guide: any;
   modalConfirm: any;
   modaldetalle: any;
   formattedData: any[] = []; // Variable para almacenar los datos fusionados
@@ -25,8 +27,10 @@ export class BulkrequestComponent {
   totalPages: number = 0; // Total de páginas
   pageSize: number = 10; // Tamaño de la página (puedes cambiarlo según tus necesidades)
   pagesArray: number[] = []; // Array para las opciones del select
+  listBase: any[] = [];
   ListProduct: any;
-
+  searchTerm$ = new Subject<any>();
+  searchTerm: string = ''; // Para almacenar el texto de búsqueda
   constructor(private _general: GeneralService, private _toast: ToastService) {}
   ngOnInit(): void {
     this.modalConfirm = new bootstrap.Modal(
@@ -41,46 +45,6 @@ export class BulkrequestComponent {
     this.get(this.currentPage);
   }
 
- // Método para obtener datos con paginación
-//  get(page: number) {
-//   this._general.getConsultantsTransp(page).subscribe((response: any) => {
-//     console.log(response);
-//     this.detailData = response.data.items; // Datos de la tabla
-//     this.totalPages = response.data.meta.totalPages; // Total de páginas
-//     this.currentPage = page; // Actualizar la página actual
-//   });
-// }
-
-// // Método para cambiar a la página anterior
-// previousPage() {
-//   if (this.currentPage > 0) {
-//     this.get(this.currentPage - 1);
-//   }
-// }
-
-// // Método para cambiar a la página siguiente
-// nextPage() {
-//   console.log(this.currentPage , this.totalPages)
-//   if (this.currentPage < this.totalPages ) {
-//     this.get(this.currentPage + 1);
-//   }
-// }
-
-// // Método para ir a una página específica
-// goToPage(page: number) {
-//   if (page >= 0 && page < this.totalPages) {
-//     this.get(page);
-//   }
-// }
-// getPageNumbers(): number[] {
-//   const pages: number[] = [];
-//   for (let i = 0; i < this.totalPages; i++) {
-//     pages.push(i);
-//   }
-//   return pages;
-// }
-
-
   // Método para obtener datos con paginación
   get(page: number) {
     this._general.getConsultantsTransp(page).subscribe((response: any) => {
@@ -88,9 +52,11 @@ export class BulkrequestComponent {
 
       
       this.detailData = response.data.items; // Datos de la tabla
+      this.listBase = this.detailData;
       this.totalPages = response.data.meta.totalPages; // Total de páginas
       this.currentPage = page; // Actualizar la página actual
       this.generatePagesArray(); // Generar el array de páginas para el select
+      this.search();
     });
   }
 
@@ -242,7 +208,7 @@ export class BulkrequestComponent {
     this.modalConfirm.show();
   }
 
-  guide: any;
+ 
   updateGuide() {
     // Validar que this.guide no sea nulo o indefinido
     if (!this.guide) {
@@ -265,5 +231,15 @@ export class BulkrequestComponent {
         // Restablecer el estado de actualización
       }
     );
+  }
+  search(): void {
+    this.searchTerm$.subscribe(({ value }: { value: string }) => {
+      this.detailData = this.listBase.filter(item => {
+        const itemValues = Object.values(item);
+        return itemValues.some(item =>
+          String(item).toLowerCase().includes(value.toLowerCase()),
+        );
+      });
+    });
   }
 }
