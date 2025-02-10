e551973b71209e18f5a226874752fcffbbb1466b
luis torres <luistorbul2002@gmail.com>
2025-02-10 09:24:00 -0500

    avances modulo de cargue de archivo

--- src/app/core/services/general.service.ts
+++ src/app/core/services/general.service.ts
@@ -7,10 +7,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GeneralService {
 
-  constructor(private _api: HttpClient) {}
+  constructor(private _api: HttpClient, private _service:ApiService) {}
 
 	getMenu(): Observable<any> {
     return this._api.get<any>('assets/json/menu.json');
   }
+
+
+    // Método para enviar el archivo a la ruta especificada
+    uploadFile(file: File): Observable<any> {
+      const formData = new FormData();
+      formData.append('file', file); // 'file' es el nombre del campo que espera el backend
+  
+      // Enviar el archivo a la ruta especificada
+      return this._service.post('registro', formData);
+    }
+
+    // Obtener el listado de asesores con opción de paginación
+    getConsultantsTransp(params: any): Observable<any> {
+      return this._service.get(`registro?page=${params}`);
+    }
+
+    UpdateGuia(params: any): Observable<any> {
+      return this._service.patch(`registro/guia/${params}`);
+    }
 
 }
